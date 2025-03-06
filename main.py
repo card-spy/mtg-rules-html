3dca5d1dab4f49fbd9b5e66d7c0e94aafadf10bd
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-06 23:31:47 +1300

    Add simple markdown parser

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+TOP_LEVEL_HEADINGS = [
+  "Magic: The Gathering Comprehensive Rules",
+]
+
+SUB_LEVEL_HEADINGS = [
+  "Introduction",
+  "Contents",
+  "Glossary",
+  "Credits",
+]
 
 def getRulesURL():
   url = 'https://magic.wizards.com/en/rules'
@@ -15,33 +25,61 @@ def getRulesURL():
 
   return rules_url
 
-
 def downloadRulesText(url):
   response = requests.get(url)
 
-  return response.content
+  return response.content.decode('utf-8-sig')
 
+def parseLineFromRules(line):
+  if line in TOP_LEVEL_HEADINGS:
+    return '# ' + line + '\n'
+  elif line in SUB_LEVEL_HEADINGS:
+    return '## ' + line + '\n'
+  else:
+    return line + '\n'
 
-def parseRulesTextIntoMarkdown():
-  return "TBC"
+def parseRulesTextIntoMarkdown(rules_text):
+  markdown_rules = ""
 
+  count = 0
+  skip_contents_list = False
+
+  for line in rules_text.splitlines():
+    if (skip_contents_list):
+      if (line.strip() == "Credits"):
+        skip_contents_list = False
+      continue
+
+    if (count == 100):
+      break
+
+    markdown_rules += parseLineFromRules(line.strip())
+
+    if (line.strip() == "Contents"):
+      skip_contents_list = True
+
+    count += 1
+  return markdown_rules.strip()
 
 def createHTMLFromMarkdown():
   return "TBC"
 
-
 if __name__ == '__main__':
   """
-    1. Scrape the rules page to get a link to the text version of the rulebook
-    2. Download the text version of the rulebook
-    3. Parse the text version of the rulebook into markdown
-    4. Convert the markdown into html
-    5. Write the html to a file
-    """
+  1. Scrape the rules page to get a link to the text version of the rulebook
+  2. Download the text version of the rulebook
+  3. Parse the text version of the rulebook into markdown
+  4. Convert the markdown into html
+  5. Write the html to a file
+  """
 
   rules_text_url = getRulesURL()
 
   rules_text = downloadRulesText(rules_text_url)
 
-  with open('rules.txt', 'wb') as f:
-    f.write(rules_text)
+  markdown_rules = parseRulesTextIntoMarkdown(rules_text)
+
+  # html_rules = createHTMLFromMarkdown(markdown_rules)
+
+  # with open('rules.html', 'w') as f:
+  #   f.write(html_rules)
